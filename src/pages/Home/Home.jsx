2051772cda6d7c02a504fe37ec9e6a92e43c3504
Md Parvez Ahamed <devparvez23@gmail.com>
2023-11-17 10:33:06 +0600

    font setup done

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+
+
+const Home = () => {
+    return (
+        <div>
+            <h1 className="text-center font-poppins font-bold text-6xl mt-4">THis is a home </h1>
+        </div>
+    );
+};
+
+export default Home;
